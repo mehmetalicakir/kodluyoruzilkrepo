f1c5d343c1994b96cb5a4b898f8482cb7c922124
mehmetalicakir <cakiroglu_m.ali@hotmail.com>
2021-12-26 15:26:35 +0300

    clock added

--- javascriptclock/js/clock.js
+++ javascriptclock/js/clock.js
@@ -1,1 +1,13 @@
-document.querySelector("#myName").innerHTML = prompt("İsminizi Giriniz: ");
+// document.querySelector("#myName").innerHTML = prompt("İsminizi Giriniz: ");
+
+function showTime() {
+    const today = new Date().getTime();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    document.getElementById("myClock").innerText = h;
+    document.getElementById("myClock").textContent = h;
+    setTimeout(showTime, 1000);
+  }
+
+  showTime();
