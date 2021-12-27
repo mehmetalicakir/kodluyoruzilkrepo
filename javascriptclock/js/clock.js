358f187cdbd399f7d5315e078bfdafaef93026b8
mehmetalicakir <cakiroglu_m.ali@hotmail.com>
2021-12-27 10:30:04 +0300

    fixed clock and added date

--- javascriptclock/js/clock.js
+++ javascriptclock/js/clock.js
@@ -1,13 +1,39 @@
-// document.querySelector("#myName").innerHTML = prompt("İsminizi Giriniz: ");
+document.querySelector("#myName").innerHTML = prompt("İsminizi Giriniz: ");
 
-function showTime() {
-    const today = new Date().getTime();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    document.getElementById("myClock").innerText = h;
-    document.getElementById("myClock").textContent = h;
-    setTimeout(showTime, 1000);
+function showTime(){
+  var date = new Date();
+  var d = date.getDay();
+  var h = date.getHours(); 
+  var m = date.getMinutes(); 
+  var s = date.getSeconds(); 
+  
+
+  switch(d){
+    case 1: d = "Pazartesi";
+    break;
+    case 2: d = "Salı";
+    break;
+    case 3: d = "Çarşamba";
+    break;
+    case 4: d = "Perşembe";
+    break;
+    case 5: d = "Cuma";
+    break;
+    case 6: d = "Cumartesi";
+    break;
+    case 7: d = "Pazar";
   }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + "  " + d;
+  document.getElementById("myClock").innerText = time;
+  document.getElementById("myClock").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
 
-  showTime();
+showTime();
