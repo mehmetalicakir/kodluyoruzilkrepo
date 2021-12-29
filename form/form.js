00d45dac3df10a7c3952b9ea8c850c5977e9e81e
mehmetalicakir <cakiroglu_m.ali@hotmail.com>
2021-12-29 13:12:08 +0300

    added functions

--- form/form.js
+++ form/form.js
@@ -1,10 +1,35 @@
-let formDom = document.querySelector("#userForm");
-formDom.addEventListener('submit', formSubmit);
+let userFormDom = document.querySelector("#userForm");
+userFormDom.addEventListener('submit', formHandler);
 
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
 
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
 
-function formSubmit(event){
+  alertPlaceholder.append(wrapper)
+}
+
+function formHandler(event){
     event.preventDefault();
-    let scoreInfoDOM = document.querySelector("#score");
-    console.log(scoreInfoDOM.value);
+    const USER_NAME = document.querySelector("#username");
+    const SCORE = document.querySelector("#score");
+    if(USER_NAME.value && SCORE.value){
+    addItem(USER_NAME.value , SCORE.value);
+    USER_NAME.value =""
+    SCORE.value =""
+    }else{
+        alert('Eksik Giriş Yapıldı', 'danger')
+    }
+}
+
+
+let userListDOM = document.querySelector("#userList");
+
+const addItem = (username,score) => {
+    let liDOM = document.createElement("li");
+    liDOM.innerHTML= `${username}<span class="badge bg-primary rounded-pill">${score}</span>`;
+    liDOM.classList.add("list-group-item" ,"d-flex" , "justify-content-between" , "align-items-center")
+    userListDOM.append(liDOM);
 }
